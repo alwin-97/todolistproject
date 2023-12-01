276211a871bb96ee68efc8d21cd0ed04f9ae6664
Alwin-91 <alwinjosephkaduppil@gmail.com>
2023-12-01 15:42:51 +0530

    updates with adding description

--- todolist/models.py
+++ todolist/models.py
@@ -1,14 +1,14 @@
 import datetime
-
+import uuid
 from django.db import models
 
 # Create your models here.
 class ToDoList(models.Model):
-    taskid = models.UUIDField(auto_created=True, primary_key=True)
+    taskid = models.UUIDField(auto_created=True, primary_key=True,default=uuid.uuid4)
     task_title = models.TextField(max_length=255,blank=False)
     task_description = models.TextField(null=True)
     task_created_on = models.DateTimeField(default=datetime.datetime.now())
-    task_completed_on = models.DateTimeField()
+    task_completed_on = models.DateTimeField(blank=True,null=True)
 
-    def __str__(self):
-        return self.task_title[:50]
+    # def __str__(self):
+    #     return self.task_title[:50]
--- todolist/views.py
+++ todolist/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 
-# will store the tasks that are added
-todolist = []
-
+from .models import ToDoList
 
 # Create your views here.
 def index(request):
     if request.POST:
-        new_task = request.POST['task']
-        todolist.append(new_task.strip())
+        new_task = request.POST['task'].strip()
+        new_desc = request.POST['description']
+        todolist = ToDoList(task_title=new_task,task_description=new_desc)
+        todolist.save()
         return redirect('index')
 
+    todolist = ToDoList.objects.all()
     return render(request,
                   'todolist/index.html',
                   context={'tasks': todolist})
