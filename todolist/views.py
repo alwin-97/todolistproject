677da647af8dacae7c3149e5dca4493c59e74e5b
Alwin-91 <alwinjosephkaduppil@gmail.com>
2023-12-12 12:16:05 +0530

    updates with edit operations

--- todolist/views.py
+++ todolist/views.py
@@ -27,6 +27,14 @@ def deleteTask(request, id):
 def editTask(request, id):
     task = ToDoList.objects.get(taskid=id)
 
+    if request.POST:
+        new_task = request.POST['task'].strip()
+        new_desc = request.POST['description']
+        task.task_title = new_task
+        task.task_description = new_desc
+        task.save()
+        return redirect('index')
+
     return render(request,
                   'todolist/edit-todo.html',
                   context={'task': task})
